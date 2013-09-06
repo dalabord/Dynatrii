be073a1f998c1160956038ff924bad1cf16773d0
dalabord <dalabord@hotmail.com>
2013-09-06 17:16:31 +0200

    v2
    - Dialog boxes implemented
    - New, Move & Delete actions implemented
    - Interface refresh

--- js/dynatrii.js
+++ js/dynatrii.js
@@ -2,35 +2,80 @@
 $(function() {
 
 	jsonDossiers = $.parseJSON($('#dynatrii').attr('data-dyna'));
-	
+	var sOK = "OK";
+	var sNOK = "Cancel";
+	var sTitle = "Tree edition";
+	var sMess = "";
+
+    $("#dynatriiModal").dialog({
+    	modal: true,
+        position: 'center',
+		autoOpen : false,
+		show : {
+			effect : "blind",
+			duration : 1000
+		},
+		hide : {
+			effect : "blind",
+			duration : 1000
+		}
+	});
+
 	$("#dynatrii").dynatree({
 		minExpandLevel : 1,
 		children : jsonDossiers,
 		onActivate : function(node) {
-			//$("#modal_ajax_loader").dialog("open");
-			// var current_node = node.data.key;
-			fc_dynatriiAjax("SELECT");
+			fc_dynatriiAjax("SELECT", false);
 		}
 	});
 	
 	$("#dynatriiCreateNode").click(function() {
-		fc_dynatriiAjax("CREATE");
+		sMess = "Name of the new node";
+		gc_openDynatriiModal("CREATE", sTitle, sMess, sOK, sNOK);
 	});
 
 	$('#dynatriiMoveNode').click(function() {
-		fc_dynatriiAjax("MOVE");
+		sMess = "Select the destination of the node";
+		gc_openDynatriiModal("MOVE", sTitle, sMess, sOK, sNOK);
 	});
 
 	$('#dynatriiDeleteNode').click(function() {
-		fc_dynatriiAjax("DELETE");
+		sMess = "Do you confirm supression ?";
+		gc_openDynatriiModal("DELETE", sTitle, sMess, sOK, sNOK);
 	});
+	
+	$('#dynatriiModal_ok').click(function() {
+		fc_switchDynatrii();
+	});
+	
+	$('#dynatriiModal_nok').click(function() {
+		$("#dynatriiModal").dialog("close");
+	});
+	
+	// $(".bs-cadre").append("<div class=\"bs-cadre-label\">Information</div>");
+	
+	/* A l'init, on sélectionne le dossier racine utilisateur et on affiche la liste de ses groupes/études
+	noeud_selectionne = $("#tree").dynatree("getTree").getNodeByKey(serveur_dossier_selec);
+	
+	// window.console.log("noeud_selectionne = "+ noeud_selectionne +" pour serveur_dossier_selec : "+ serveur_dossier_selec);
+	if (noeud_selectionne != null)		
+		noeud_selectionne.activate();
+	*/
+	// Petite gêne à l'affichage : mention close au dessus des popup
+	$(".ui-dialog-titlebar").hide();
+
 });
 
-function fc_dynatriiAjax(sAction) {
+function fc_dynatriiAjax(sAction, bDialogBox) {
 	var controllerAction = $('#dynatrii').attr('controller-action');
 	var current_node = $('#dynatrii').dynatree('getActiveNode');
 	
-	// alert(controllerAction+" : "+current_node.data.key +" & "+sAction);
+	if (current_node == null) {
+		
+	}
+
+	if (bDialogBox) $("#dynatriiModal").dialog("open");
+	
 	$.ajax({
 		url : controllerAction,
 		type : "POST",
@@ -40,7 +85,6 @@ function fc_dynatriiAjax(sAction) {
 			sCRUD : sAction
 		},
 		success : function(jsonArray) {
-			// jsonArray : 0=>id, 1=>errCode, 2=>toPrinf
 			var lstElements = jQuery.parseJSON(jsonArray); 
 
 			if (lstElements.errCode >= 0) {
@@ -55,4 +99,101 @@ function fc_dynatriiAjax(sAction) {
 			alert("Dynatrii ("+ sAction +")- Status : " + textStatus+ " / Error : " + errorThrown);
 		}
 	});
+}
+
+function gc_openDynatriiModal($sCRUD, $sTitle, $sMess, $sOK, $sNOK) {
+	$("#dynatriiModal_message").empty();
+	$("#dynatriiModal_title").empty();
+	$("#dynatriiModal_special").empty();
+	
+	if ($sCRUD == "MOVE") {		
+		fc_extendAllNodes();
+		$("#dynatriiModal_special").append("<div id=\"dynatrii-move\"></div>");
+		var dict = $("#dynatrii").dynatree("getTree").toDict(true, function(dict) {
+			dict.title = "Target";
+			delete dict.key; // prevent duplicate keys
+		});
+		var arbre_cible_avant = JSON.stringify(dict);
+		var arbre_cible_apres = arbre_cible_avant.substring(arbre_cible_avant
+				.indexOf("[", 2), arbre_cible_avant.length - 1);
+
+		var jsonDossiers = $.parseJSON(arbre_cible_apres);
+
+		$("#dynatrii-move").dynatree({
+			minExpandLevel : 2,
+			children : jsonDossiers,
+			onActivate : function(node) {
+				$("#dynatriiModal").attr("target-node", node.data.key);
+			}
+		});
+	}
+	if ($sCRUD == "CREATE") {	
+		$("#dynatriiModal_special").append("<input type=\"text\" id=\"dynatriiModal_input\" value=\"\"></input>");
+	}
+	$("#dynatriiModal_title").append($sTitle);
+	$("#dynatriiModal_message").append("<p>" + $sMess + "</p>");
+	$("#dynatriiModal_ok").text($sOK);
+	$("#dynatriiModal_nok").text($sNOK);
+	$("#dynatriiModal").attr("value", $sCRUD);
+	$("#dynatriiModal").dialog("open");
+}
+
+function fc_switchDynatrii() {
+	
+	switch ($("#dynatriiModal").attr("value")) {
+		case "CREATE" :
+			fc_addNode($("#dynatriiModal_input").val(), $("#dynatriiModal_input").val());
+			break;
+		case "MOVE" :
+			fc_moveNode();
+			break;
+		case "DELETE" :
+			fc_deleteNode();
+			break;
+		default :
+			break;
+	}	
+	
+	// eventualy, access to server
+}
+
+function fc_addNode(idNode, libelNode) {
+	// Now get the current node object
+	var targetNode = $("#dynatrii").dynatree("getActiveNode");
+	// Call the DynaTreeNode.addChild() member function and pass options for the new node
+	targetNode.addChild({
+		key : idNode,
+		title : libelNode,
+		tooltip : "Node added successfully",
+		isFolder : true
+	});
+	targetNode.expand(true);
+	$("#dynatriiModal").dialog("close");
+
+	fc_dynatriiAjax("CREATE", false);
+}
+
+function fc_moveNode() {
+	var originNode = $("#dynatrii").dynatree("getActiveNode");
+	var targetNode = $("#dynatrii").dynatree("getTree").getNodeByKey($("#dynatriiModal").attr("target-node"));
+	originNode.move(targetNode, 'child');
+	$("#dynatrii").dynatree("getTree").activateKey($("#dynatriiModal").attr("target-node"));
+	$("#dynatriiModal").dialog("close");
+	
+	fc_dynatriiAjax("MOVE", false);
+}
+
+function fc_deleteNode() {
+	var parentNode = $("#dynatrii").dynatree("getActiveNode").getParent();
+	var originNode = $("#dynatrii").dynatree("getActiveNode");
+	originNode.remove();
+	$("#dynatriiModal").dialog("close");
+	$("#dynatrii").dynatree("getTree").activateKey(parentNode.data.key);
+	fc_dynatriiAjax("DELETE", false);
+}
+
+function fc_extendAllNodes() {
+	$("#dynatrii").dynatree("getRoot").visit(function(node){
+		node.expand(true);
+	});	
 }
--- views/body.php
+++ views/body.php
@@ -10,13 +10,14 @@
 	Yii::app()->clientScript->registerScriptFile($extPath .'js/dynatrii.js');
 	Yii::app()->clientScript->registerCssFile($extPath .'css/dynatrii.css');
 	
-	// DIV contenant l'arbre dynatree
-echo '<div class="dynatrii-container">';
+// DIV contenant l'arbre dynatree
+	echo '<div class="dynatrii-container">';
+
 	echo '<div id="dynatrii" data-dyna="'. htmlspecialchars(json_encode($this->dataDyna)) .'" '.
 						' field-to-refresh="'. $this->fieldToRefresh .'" '.
 						' controller-action="'. $this->controllerAction .'"> </div>';
 
-	// Affichage des boutons d'édition Création, Deplacement, Suppression
+// Affichage des boutons d'édition Création, Deplacement, Suppression
 	echo '<div class="dynatrii-buttons">';
 	echo '	<div class="bg_create_img"> '.
 	 	'		<a class="click_field" id="dynatriiCreateNode" title="New"></a> '.
@@ -28,6 +29,23 @@ echo '<div class="dynatrii-container">';
 		'		<a class="click_field" id="dynatriiDeleteNode" title="Delete"></a>'.
 		'	</div>';
 	echo '</div>';
-echo '</div>';
-echo '<div class="dynatrii-separation"></div>';
+
+// Modal
+	echo '<div id="dynatriiModal" class="bs-cadre" label="Info" value="">'.
+			 '<div class="modal-header">'.
+			 	'<h3 id="dynatriiModal_Label"><div id="dynatriiModal_title">Information</div></h3>'.
+		 	 '</div>'.
+		 	 '<div class="modal-body">'.
+				 '<div id="dynatriiModal_message"></div>'.
+				 '<div id="dynatriiModal_special"></div>'.
+			 '</div>'.
+			 '<div class="modal-footer">'.
+				 '<button id="dynatriiModal_ok" class="btn btn-primary">OK</button>'.
+				 '<button id="dynatriiModal_nok" class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>'.
+			 '</div>'.
+		 '</div>';
+
+	echo '</div>';
+
+	echo '<div class="dynatrii-separation"></div>';
 ?>
